bdc52958e5dbe22ed54587d5cfa111269cfb201f
Raheemspb <gabibovda@gmail.com>
2024-05-05 17:24:12 +0300

    Create firstViewController, secondViewController, navigationBar, tabBar

---
+++ Electrolux final/Electrolux final/SecondTabViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondTabViewController.swift
+//  Electrolux final
+//
+//  Created by Рахим Габибли on 05.05.2024.
+//
+
+import UIKit
+
+class SecondTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Electrolux final/Electrolux final/TabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarController.swift
+//  Electrolux final
+//
+//  Created by Рахим Габибли on 05.05.2024.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViewControllers()
+    }
+    private func setupViewControllers() {
+        viewControllers = [generateViewController(viewController:
+                                                    FirstTabViewController(),
+                                                  image: UIImage(systemName: "1.square.fill"),
+                                                  title: "First Tab"),
+                           generateViewController(viewController:
+                                                    SecondTabViewController(),
+                                                  image: UIImage(systemName: "2.square.fill"),
+                                                  title: "Second Tab")
+        ]
+    }
+    
+    private func generateViewController(viewController: UIViewController, image: UIImage?, title: String) -> UIViewController {
+        viewController.tabBarItem.image = image
+        viewController.tabBarItem.title = title
+        return viewController
+    }
+
+}
